a8f82bec7223fd77650cabb37099d48fad6eede0
Chiranjit <cmandal2003@gmail.com>
2024-02-02 21:46:55 +0530

    Add Timeline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import Sponsors from "./components/Sponsors";
+import Timeline from "./components/Timeline";
 
 export default function App(){
     return(
         <>
         <Hero/>
+        <Timeline/>
         <Sponsors/>
         </>
     )
