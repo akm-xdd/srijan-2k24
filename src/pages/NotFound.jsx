005451b11cb0f1c60d8a2ebe333733cdec68aff6
Chiranjit <cmandal2003@gmail.com>
2024-02-07 20:56:06 +0530

    Add 404 Page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function NotFound(props) {
+    return (
+        <div className="flex flex-col md:flex-row bg-soothing_black items-center justify-center h-screen w-screen p-4 text-white">
+            <div className="flex flex-col gap-3">
+                <h1 className="text-lg md:text-xl font-medium font-ibm">
+                    {" "}
+                    <b className="text-3xl">Oops</b> page is not found !
+                </h1>
+                <p className="text-sm font-medium font-ibm">
+                    You just hit a route that doesn't exist...
+                </p>
+                <a
+                    className="text-sm font-medium font-ibm text-main_primary"
+                    href="/"
+                >
+                    Go back
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
