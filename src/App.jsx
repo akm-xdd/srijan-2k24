70d8b6debb1fab902f7f9b82ea86282f9240538a
Chiranjit <cmandal2003@gmail.com>
2024-02-07 20:59:41 +0530

    Add Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Events from "./pages/Events";
+import About from "./pages/About";
+import Team from "./pages/Team";
+import Sponsors from "./pages/Sponsors";
+import NotFound from "./pages/NotFound";
 
-export default function App(){
-    return(
-        <BrowserRouter>
+export default function App() {
+  return (
+    <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Layout />}> */}
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="events" element={<Events />} />
-          {/* <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
+          <Route path="about" element={<About />} />
+          <Route path="team" element={<Team />} />
+          <Route path="sponsors" element={<Sponsors />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-    )
+  )
 }
 
