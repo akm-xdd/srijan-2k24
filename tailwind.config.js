0097a2e70e5efcfe0d5b2d4ea105e8e7e5ca4574
Chiranjit <cmandal2003@gmail.com>
2024-02-07 21:00:26 +0530

    Add Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,30 @@ export default {
   ],
   theme: {
     extend: {
-        colors: {
-          'forest':'#228B22',
-          'mint':'#F4FFF8',
-          'english':'#4C3B4D',
-        },
-        fontFamily: {
-          'dreamland': 'Dreamland',
-        },
-        animation: {
-          'slide': 'slide 3s linear infinite',
-          'playstate': 'running'
-        },
-        keyframes: {
-          slide: {
-            '0%': {transform: 'translateX(20%)'},
-            '100%': {transform: 'translateX(-20%)'}
-          }
+      colors: {
+        'forest': '#228B22',
+        'mint': '#F4FFF8',
+        'english': '#4C3B4D',
+        'soothing_black': "#212121",
+        'main_primary': "#7cb342",
+        'primary': "#1E0B37",
+        'gray': "#868888",
+        'footer': "#1B1B1B",
+
+      },
+      fontFamily: {
+        'dreamland': 'Dreamland',
+      },
+      animation: {
+        'slide': 'slide 3s linear infinite',
+        'playstate': 'running'
+      },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(20%)' },
+          '100%': { transform: 'translateX(-20%)' }
         }
+      }
     },
   },
   plugins: [],
