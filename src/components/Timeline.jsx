be128d5e0b30af84a3017a8c7fb01679ed4c7982
Chiranjit <cmandal2003@gmail.com>
2024-03-03 23:15:40 +0530

    Dynamic Events

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import "./Timeline.css";
 import EventCard from './EventCard';
 
-const events = [
-    {
-        title: "Zami-Parasti",
-        img: "/assets/events/zami-parasti.jpg",
-        link: "https://docs.google.com/forms/d/e/1FAIpQLSfCIahMt7vGDrS8fNVsHxh7wooTDxBvrQzmoaok6rBrIrbbKw/viewform?pli=1"
-    }, 
-    {
-        title: "Yogasana",
-        img: "/assets/events/yogasana.jpg",
-        link: "https://docs.google.com/forms/d/e/1FAIpQLSfCIahMt7vGDrS8fNVsHxh7wooTDxBvrQzmoaok6rBrIrbbKw/viewform?pli=1"
-    }
-]
-
 function Timeline(props) {
+
+    const [data, setData] = useState({
+        events: [],
+        loaded: false,
+    });
+
+    useEffect(() => {
+        fetch(`https://storage.googleapis.com/srijan-2k24.appspot.com/events.json?v=${Date.now()}`)
+            .then((res) => res.json())
+            .then((json) => {
+                setData({
+                    events: json,
+                    loaded: true,
+                });
+            });
+    }, []);
+
     return (
         <div className="timeline flex flex-col items-center justify-center h-fit pb-[80px] pt-10">
             <h1 className="text-[2.7rem] leading-10 text-center md:text-[4rem] text-english font-clash font-bold tracking-wider">
-            EVENTS
-          </h1>
+                EVENTS
+            </h1>
             <div className="flex flex-wrap gap-12 md:gap-20 lg:gap-32 pt-10 items-center justify-center px-10 mt-10 flex-row relative">
 
-                {/* <div style={{
-                    height: "90%",
-                    position: "absolute",
-                    width: "25px",
-                    backgroundColor: "white",
-                    borderRadius: "25px"
-                }} /> */}
-                
-                {events.map((event, index) => (
+                {data.loaded && data.events.map((event, index) => (
                     <a href={event.link} key={index}>
-                    <EventCard title={event.title} img={event.img}  />
+                        <EventCard title={event.title} img={event.img} />
                     </a>
                 ))}
+
+                {
+                    !data.loaded && (
+                        <div className="flex justify-center items-center h-[75vh]">
+                            <div className="animate-spin rounded-full h-32 w-32 border-b-4 border-[#7cb342]"></div>
+                        </div>
+                    )
+                }
 
             </div>
         </div>
