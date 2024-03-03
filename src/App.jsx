99a6e5e74b55a6cc2e1f116fcef70c3e0087d23e
Chiranjit <cmandal2003@gmail.com>
2024-03-03 22:19:57 +0530

    Remove Events Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Events from "./pages/Events";
 import About from "./pages/About";
 import Team from "./pages/Team";
 import Sponsors from "./pages/Sponsors";
@@ -13,7 +12,6 @@ export default function App() {
         {/* <Route path="/" element={<Layout />}> */}
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="events" element={<Events />} />
           <Route path="about" element={<About />} />
           <Route path="team" element={<Team />} />
           <Route path="sponsors" element={<Sponsors />} />
